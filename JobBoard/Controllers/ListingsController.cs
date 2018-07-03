b60bca1c4acf9dc22f011bdad9cdb00911116cfb
David Ngo, Kyle Parsons and Ryan Putman <ryan@>
2018-07-03 10:27:22 -0700

    work

---
+++ JobBoard/Controllers/ListingsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JobBoard.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobBoard.Controllers
+{
+    public class ListingsController : Controller
+    {
+        [HttpGet("/listings")]
+        public IActionResult Index()
+        {
+            Listings newListings = new Listings();
+            newListings.SetTitle(Request.Query["job-title"]);
+            newListings.SetDescription(Request.Query["job-description"]);
+            newListings.SetContactName(Request.Query["job-contact-name"]);
+            newListings.SetContactEmail(Request.Query["job-contact-email"]);
+            newListings.SetContactPhone(Request.Query["job-contact-phone"]);
+            newListings.UpdateList();
+            return View(newListings.GetList());
+        }
+    }
+}
---
+++ JobBoard/Models/Listings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobBoard.Models
+{
+    public class Listings
+    {
+        private string _title;
+        private string _description;
+        private string _contactName;
+        private string _contactEmail;
+        private string _contactPhone;
+        private static List<Listings> _jobListings = new List<Listings>() { };
+
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public void SetTitle(string newTitle)
+        {
+            _title = newTitle;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+
+        public string GetContactName()
+        {
+            return _contactName;
+        }
+
+        public void SetContactName(string newContactName)
+        {
+            _contactName = newContactName;
+        }
+
+        public string GetContactEmail()
+        {
+            return _contactEmail;
+        }
+
+        public void SetContactEmail(string newContactEmail)
+        {
+            _contactEmail = newContactEmail;
+        }
+
+        public string GetContactPhone()
+        {
+            return _contactPhone;
+        }
+
+        public void SetContactPhone(string newContactPhone)
+        {
+            _contactPhone = newContactPhone;
+        }
+
+        public void UpdateList() 
+        {
+            _jobListings.Add(this);
+        }
+
+        public List<Listings> GetList() 
+        {
+            return _jobListings;
+        }
+    }
+}
